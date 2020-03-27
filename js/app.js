0790f523f065ab8ca97aa80111caf5b5896d0522
Hubert102536 <52202565+Hubert102536@users.noreply.github.com>
2020-03-27 14:32:28 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,29 @@
+/*
+  @TODO : Add Script
+
+  Details;
+  1. Formula for calculating IDW by
+      - Male:	 50.0 kg + 2.3 kg per inch per inch over 5 feet : B. J. Devine Formula (1974)
+      - Female:	49 kg + 1.7 kg per inch over 5 feet : J. D. Robinson Formula (1983)
+  2. BMI Formula : kg/(height in metres * height in metres)
+  3. jQuery for modifying bound name result(color change)
+  4. Enable tooltips
+
+*/
+function calculate()
+{
+	// alert('Enter the details correctly');
+	var number1 = document.getElementById('age').value;
+	var number1 = document.getElementById('height').value;
+
+	result= [50+((2.3*height)/5)];
+	var result_span = document.getElementById('results');
+	result_span.innerHTML = result;
+}
+
+// //eventListener methods
+var calculate_BMI = document.getElementById('calculateBMI');
+
+calculate_BMI.addEventListener('Click',function(){
+	alert('Not implemented also!');
+});
